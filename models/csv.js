dbe0d39fabb9814221fac7631a34d7586ee8a65e
Abhay 076 <yadavabhaycs@gmail.com>
2022-08-01 12:37:54 +0530

    initial commit

---
+++ models/csv.js
@@ -0,0 +1,16 @@
+// hhh/
+const mongoose = require('mongoose');
+
+const csvSchema = new mongoose.Schema({
+  FirstName: {
+    type: String
+  },
+  LastName: {
+    type: String
+  },
+  age: {
+    type: Number
+  }
+});
+
+module.exports = mongoose.model('personaldata', csvSchema);
